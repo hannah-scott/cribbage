467836590ac8b03a8751bb2cf6cd04f569346f44
Hannah Scott <hannahscott@tutanota.com>
2021-07-10 17:26:36 +0100

    Adds icons and favicon

---
+++ make_icons.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+inkscape --file icon.svg --export-png "cribbage_112.png" --export-width 112
+inkscape --file icon.svg --export-png "cribbage_56.png" --export-width 56
+
+inkscape --file icon.svg --export-png "icons/icon128x128.png" --export-width 128
+inkscape --file icon.svg --export-png "icons/icon60x60.png" --export-width 60
+inkscape --file icon.svg --export-png "icons/icon48x48.png" --export-width 48
+inkscape --file icon.svg --export-png "icons/icon16x16.png" --export-width 16
